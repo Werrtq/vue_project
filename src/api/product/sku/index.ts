ad6f94ee1e7caff3e3f6161bd6c411c71028ef6e
Werrtq <554480489@qq.com>
2024-08-16 20:03:38 +0800

    SKU大模块完成。

--- src/api/product/sku/index.ts
+++ src/api/product/sku/index.ts
@@ -5,7 +5,8 @@ enum API {
     GETSKULISTBYPAGE_URL = '/admin/product/list/',
     SETSKUONSALE_URL = '/admin/product/onSale/',
     SETSKUCANCELSALE_URL = '/admin/product/cancelSale/',
-    DELETESKU_URL = '/admin/product/deleteSku/'
+    DELETESKU_URL = '/admin/product/deleteSku/',
+    GETSKUINFO_URL = '/admin/product/getSkuInfo/'
 }
 
 export const reqGetSkuListByPage = (pageNo: number, limit: number) => request.get<any, skuListResponseBody>(API.GETSKULISTBYPAGE_URL+`${pageNo}/${limit}`)
@@ -15,3 +16,5 @@ export const reqSetSkuOnSale = (skuId: number) => request.get<any, any>(API.SETS
 export const reqSetSkuCancelSale = (skuId: number) => request.get<any, any>(API.SETSKUCANCELSALE_URL + skuId)
 
 export const reqDeleteSku = (skuId: number) => request.delete<any, any>(API.DELETESKU_URL + skuId)
+
+export const reqGetSkuInfo = (skuId: number) => request.get<any, any>(API.GETSKUINFO_URL + skuId)
--- src/views/product/sku/index.vue
+++ src/views/product/sku/index.vue
@@ -15,7 +15,7 @@
                 <template # = "{row, $index}">
                     <el-button type = "primary" size = "small" :icon = "row.isSale ? 'Bottom' : 'Top'" @click = "changeSaleState(row)"></el-button>
                     <el-button type = "primary" size = "small" icon = "Edit" @click = "edit"></el-button>
-                    <el-button type = "primary" size = "small" icon = "InfoFilled"></el-button>
+                    <el-button type = "primary" size = "small" icon = "InfoFilled" @click = "showDetail(row)"></el-button>
                     <el-popconfirm title="确定删除?" @confirm = 'deleteSku(row)'>
                         <template #reference>
                             <el-button type = "primary" size = "small" icon = "Delete"></el-button>
@@ -38,25 +38,61 @@
             />
     </div>
 
-    <el-drawer
-        v-model="table"
-        title="I have a nested table inside!"
-        direction="rtl"
-        size="50%"
-    >
+    <el-drawer v-model="table">
+             <template #header>
+                <h4>查看商品的详情</h4>
+            </template>
+        <template #default>
+            <el-row class = "elrow">
+            <el-col :span="6">名称</el-col>
+            <el-col :span="18">{{ skuInfo.skuName }}</el-col>
+        </el-row>
+        <el-row class = "elrow">
+            <el-col :span="6">描述</el-col>
+            <el-col :span="18">{{ skuInfo.skuDesc }}</el-col>
+        </el-row>
+        <el-row class = "elrow">
+            <el-col :span="6">价格</el-col>
+            <el-col :span="18">{{ skuInfo.price }}</el-col>
+        </el-row>
+        <el-row class = "elrow">
+            <el-col :span="6">平台属性</el-col>
+            <el-col :span="18">
+                <el-tag v-for = "item in skuInfo.skuAttrValueList" style="margin: 5px;">{{item.attrName}}</el-tag>
+            </el-col>
+        </el-row>
+        <el-row class = "elrow">
+            <el-col :span="6">销售属性</el-col>
+            <el-col :span="18">
+                <el-tag v-for = "item in skuInfo.skuSaleAttrValueList" style="margin: 5px;">{{ item.saleAttrName }}</el-tag>
+            </el-col>
+        </el-row>
+        <el-row class = "elrow">
+            <el-col :span="6">商品图片</el-col>
+            <el-col :span="18">
+                <el-carousel :interval="4000" type="card" height="200px" indicator-position = "outside">
+                            <el-carousel-item v-for="item in skuInfo.skuImageList" :key="item.id">
+                                <img :src="item.imgUrl" alt="" style="width:100%;height: 100%;">
+                            </el-carousel-item>
+                        </el-carousel>
+            </el-col>
+        </el-row>  
+        </template>
         
     </el-drawer>
 </template>
 
 <script lang = 'ts' setup>
     import { ref, onMounted } from 'vue';
-    import { reqGetSkuListByPage, reqSetSkuOnSale, reqSetSkuCancelSale, reqDeleteSku } from '@/api/product/sku/index'
+    import { reqGetSkuListByPage, reqSetSkuOnSale, reqSetSkuCancelSale, reqDeleteSku, reqGetSkuInfo } from '@/api/product/sku/index'
     import { ElMessage } from 'element-plus';
 
     let pageNo = ref<number>(1);
     let limit = ref<number>(7);
     let totalPage = ref<number>(0);
     let skuList = ref([]);
+    let table = ref(false);
+    let skuInfo = ref<any>({});
 
     const getSkuList = async (pageNo: number, limit: number) => {
         let result = await reqGetSkuListByPage(pageNo, limit);
@@ -129,9 +165,25 @@
                 });
             }
         }
-    }   
+    }
+
+    async function showDetail(row: any) {
+        table.value = true;
+        let result = await reqGetSkuInfo(row.id);
+
+        if(result.code == 200){
+            skuInfo.value = result.data;
+        }
+    }
 
     onMounted(() => {
         getSkuList(pageNo.value, limit.value);
     });
 </script>
+
+<style scoped>
+    .elrow{
+        margin: 10px 0px;
+    }
+ 
+</style>
